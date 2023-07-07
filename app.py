8c06bb182e0bd160eb4495dfcfaa45d81e54c0ec
Monseleon21 <121038307@upq.edu.mx>
2023-07-07 15:28:20 -0600

    insert  de pedidos fundas

--- app.py
+++ app.py
@@ -36,8 +36,27 @@ def guardar():
     cs.execute('insert into tbpedido_fundas (Nombre, Ape_pa, Ape_ma, Municipio, Estado, Colonia, Calle, Direccion) values (%s,%s,%s,%s,%s,%s,%s,%s)',(VNombre,VApe_pa,VApe_ma,VMunicipio,VEstado,VColonia,VCalle,VDireccion))
     mysql.connection.commit()
 
-  flash('El pedido fue agregado exitosamente!')
-  return redirect(url_for('Menu'))
+    flash('El pedido fue agregado exitosamente!')
+    return redirect(url_for('index'))
+  return render_template('menu.html')
+
+@app.route ('/guardares', methods = ['GET','POST'])
+def guardares ():
+   if request.method == 'POST':
+      
+      VMarca = request.form ['txtMarca']
+      VModelo = request.form ['txtModelo']
+
+      cs = mysql.connection.cursor()
+      cs.execute('Insert into tbespecificaciones (Marca, Modelo) values (%s,%s)',(VMarca,VModelo))
+      mysql.connection.commit()
+
+      flash('Las especificaciones se registraron correctamente!')
+      return redirect(url_for('index'))
+
+   @app.route('/opt', methods = ['POST'])
+   def opt():
+       return render_template('Menu.html')
 
 
 if __name__ == '__main__':
