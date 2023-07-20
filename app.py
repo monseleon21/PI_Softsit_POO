3e1e93f472755bde24ea6cb6a9233ef1cefe6b72
Monseleon21 <121038307@upq.edu.mx>
2023-07-20 00:41:45 -0600

    n

--- app.py
+++ app.py
@@ -1,64 +1,48 @@
-from flask import Flask, render_template, request, redirect, url_for, flash
+#importacion del framework
+from flask import Flask,render_template,request,redirect,url_for,flash
 from flask_mysqldb import MySQL
 
-app = Flask(__name__)
-
+#Inicializacion del APP
+app = Flask (__name__)
 app.config['MYSQL_HOST']='localhost'
 app.config['MYSQL_USER']='root'
 app.config['MYSQL_PASSWORD']=''
-app.config['MYSQL_DB']='Softsit_pi'
-app.secret_key='mysecretkey'
-mysql=MySQL(app)
+app.config['MYSQL_DB']='Softsit_pii'
+app.secret_key= 'mysecrety'
+mysql= MySQL(app)
 
-
+#Declaracion de ruta http://localhost:5000
 @app.route('/')
 def index():
-    return render_template('pedidos_fundas.html')
-
+  CC= mysql.connection.cursor()
+  CC.execute('select * from pefundas')
+  conpefundas= CC.fetchall()
+  print(conpefundas)
+  return render_template('index.html',listpefundas= conpefundas)
 
 #rura http:localhost:5000/guardar tipo POST para insert
-@app.route('/guardar', methods=['GET','POST'])
-def guardar():
-  
+@app.route('/guardar',methods=['POST'])
+def guardar(): 
   if request.method == 'POST':
 
     # pasamos a variables el contenido de los input
     VNombre=request.form['txtNombre']
-    VApe_pa=request.form['txtApe_pa']
-    VApe_ma=request.form['txtApe_ma']
+    VApe_p=request.form['txtApe_p']
+    VApe_m=request.form['txtApe_m']
     VMunicipio=request.form['txtMunicipio']
     VEstado=request.form['txtEstado']
     VColonia=request.form['txtColonia']
     VCalle=request.form['txtCalle']
     VDireccion=request.form['txtDireccion']
-   
+
     #Conectar y ejecutar el insert
     cs= mysql.connection.cursor()
-    cs.execute('insert into tbpedido_fundas (Nombre, Ape_pa, Ape_ma, Municipio, Estado, Colonia, Calle, Direccion) values (%s,%s,%s,%s,%s,%s,%s,%s)',(VNombre,VApe_pa,VApe_ma,VMunicipio,VEstado,VColonia,VCalle,VDireccion))
+    cs.execute('insert into pefundas (Nombre,Ape_p,Ape_m,Municipio,Estado, Colonia, Calle, Direccion) values(%s,%s,%s,%s,%s,%s,%s,%s)',(VNombre,VApe_p,VApe_m,VMunicipio,VEstado,VColonia,VCalle,VDireccion))
     mysql.connection.commit()
 
-    flash('El pedido fue agregado exitosamente!')
-    return redirect(url_for('index'))
-  return render_template('menu.html')
+  flash('El album fue agregado correctamente')
+  return redirect(url_for('index'))
 
-@app.route ('/guardares', methods = ['GET','POST'])
-def guardares ():
-   if request.method == 'POST':
-      
-      VMarca = request.form ['txtMarca']
-      VModelo = request.form ['txtModelo']
-
-      cs = mysql.connection.cursor()
-      cs.execute('Insert into tbespecificaciones (Marca, Modelo) values (%s,%s)',(VMarca,VModelo))
-      mysql.connection.commit()
-
-      flash('Las especificaciones se registraron correctamente!')
-      return redirect(url_for('index'))
-
-   @app.route('/opt', methods = ['POST'])
-   def opt():
-       return render_template('Menu.html')
-
-
+#Ejecucion del servidor en el puerto 5000
 if __name__ == '__main__':
-    app.run(port=5000, debug=True)
+  app.run(port=5000,debug=True)
