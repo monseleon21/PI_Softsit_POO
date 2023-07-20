b86c87dc513e3d398b06dcc8cc5686b0981a4b87
Monseleon21 <121038307@upq.edu.mx>
2023-07-19 23:39:24 -0600

    .

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 app.config['MYSQL_HOST']='localhost'
 app.config['MYSQL_USER']='root'
 app.config['MYSQL_PASSWORD']=''
-app.config['MYSQL_DB']='softsit_pi'
+app.config['MYSQL_DB']='Softsit_pi'
 app.secret_key='mysecretkey'
 mysql=MySQL(app)
 
